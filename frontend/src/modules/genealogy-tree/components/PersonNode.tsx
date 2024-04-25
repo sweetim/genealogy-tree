74f14ced92edecae449ccf5d099e0d690005176c
sweetim <hosweetim@gmail.com>
2024-04-25 23:50:46 +0900

    added skeleton avatar when new person created

--- frontend/src/modules/genealogy-tree/components/PersonNode.tsx
+++ frontend/src/modules/genealogy-tree/components/PersonNode.tsx
@@ -1,12 +1,14 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import { FC } from "react";
-import { Avatar, Flex, Typography } from "antd";
+import { Avatar, Flex, Skeleton, Typography } from "antd";
 
 import { PersonMetadata } from "../model";
 
 const { Text } = Typography;
 
 const PersonNode: FC<NodeProps<PersonMetadata>> = ({ data, isConnectable }) => {
+  const isNewPerson = data.name?.includes("NEW Person")
+
   const imageUri = `https://robohash.org/${data.name}?set=set1`
   const yearOfBirth = (new Date(data.dateOfBirth || Date.now())).getFullYear()
   const yearOfDeath = data.dateOfDeath
@@ -21,6 +23,10 @@ const PersonNode: FC<NodeProps<PersonMetadata>> = ({ data, isConnectable }) => {
     2: "F"
   }
 
+  const renderAvatar = isNewPerson
+    ? <Skeleton.Avatar className="m-2 w-10" size={64} shape="circle" />
+    : <Avatar className="m-2" size={64} src={imageUri} />
+
   return (
     <div className="text-updater-node">
       <Handle type="target"
@@ -28,7 +34,7 @@ const PersonNode: FC<NodeProps<PersonMetadata>> = ({ data, isConnectable }) => {
         position={Position.Top}
         isConnectable={isConnectable} />
       <Flex align="center" justify='center' vertical className='h-full'>
-        <Avatar className="m-1" size={64} src={imageUri} />
+        {renderAvatar}
         <Text strong>{data.name} ({GENDER_TO_STRING[data.gender || 0] || ""})</Text>
         <Text type="secondary">{yearsOfLiving}</Text>
       </Flex>
